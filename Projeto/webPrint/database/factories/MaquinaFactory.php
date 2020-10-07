8a87924705c1ce859383109631f7fe55ceb9e796
Fernando <fernandosilva2011@hotmail.com>
2020-10-06 23:44:22 -0300

    Commit #31

---
+++ Projeto/webPrint/database/factories/MaquinaFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Maquina;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class MaquinaFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Maquina::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
